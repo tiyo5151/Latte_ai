8327354c039b047ac878df7ba2015e400bc71f24
hibikinggg <s1f102400808@iniad.org>
2024-06-29 03:21:03 +0900

    novel create

---
+++ server/api/novel/index.ts
@@ -0,0 +1,13 @@
+import type { DefineMethods } from 'aspida';
+
+export type Methods = DefineMethods<{
+  get: {
+    resBody: string;
+  };
+  post:{
+    reqBody:{
+      aozoraUrl:string;
+    };
+    resBody: string;
+  };
+}>;
---
+++ server/domain/novel/useCase/novelUsecase.ts
@@ -0,0 +1,9 @@
+
+import { transaction } from 'service/prismaClient';
+
+export const novelUseCase = {
+  scrape: (aozoraUrl: string): Promise<string> =>
+    transaction('RepeatableRead', async (tx) => {
+      return aozoraUrl ;
+    }),
+};
